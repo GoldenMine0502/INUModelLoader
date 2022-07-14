3ff1d4e3e60133e559e40bdef4a5ecf24f4df407
GoldenMine0502 <ehe123@naver.com>
2022-07-14 15:50:57 +0900

    sort signs in itemgroup creative tab

--- src/main/java/kr/goldenmine/inumodelloader/inumodelloader/item/ModItemGroup.java
+++ src/main/java/kr/goldenmine/inumodelloader/inumodelloader/item/ModItemGroup.java
@@ -1,7 +1,14 @@
 package kr.goldenmine.inumodelloader.inumodelloader.item;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class ModItemGroup {
     public static final ItemGroup  TUTORIAL_TAB = new ItemGroup ("tutorial_tab") {
@@ -15,6 +22,23 @@ public class ModItemGroup {
         @Override
         public ItemStack createIcon() {
             return new ItemStack(ModItems.INU_ITEM.get());
+        }
+
+        @Override
+        public void fill(NonNullList<ItemStack> items) {
+            super.fill(items);
+//            List<Item> registryItems = new ArrayList<Item>();
+//            for(Item item : Registry.ITEM) {
+//                registryItems.add(item);
+//            }
+//
+//            registryItems.sort(Comparator.comparing(o -> o.getItem().getName().toString()));
+//
+//            for(Item item : registryItems) {
+//                item.fillItemGroup(this, items);
+//            }
+
+            items.sort(Comparator.comparing(o -> o.getItem().getName().toString()));
         }
     };
 }
