e504b84e392f0bdf30a5938f6922e04ae10b8afa
GoldenMine0502 <ehe123@naver.com>
2022-08-26 21:52:33 +0900

    패킷 버전 변경

--- src/main/java/kr/goldenmine/inumodelloader/inumodelloader/network/AssetNetwork.java
+++ src/main/java/kr/goldenmine/inumodelloader/inumodelloader/network/AssetNetwork.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class AssetNetwork {
-    public static final String NETWORK_VERSION = "0.1.0";
+    public static final String NETWORK_VERSION = "0.1.1";
 
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
             new ResourceLocation(Inumodelloader.MOD_ID, "assetnetwork"),
